6e7a3e263f27d1e3edbca40623e48c4607fd40ea
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-07 01:23:10 +0800

    play around with edit text

--- app/src/main/java/com/irfanshukri203/edittexttutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/edittexttutorial/MainActivity.kt
@@ -2,10 +2,63 @@ package com.irfanshukri203.edittexttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    fun addOperator(firstNumber: Int, secondNumber: Int) {
+        val result = firstNumber + secondNumber
+
+        txtResult.text = result.toString()
+    }
+
+    fun minusOperator(firstNumber: Int, secondNumber: Int) {
+        val result = firstNumber - secondNumber
+
+        txtResult.text = result.toString()
+    }
+
+    fun multipleOperator(firstNumber: Int, secondNumber: Int) {
+        val result = firstNumber * secondNumber
+
+        txtResult.text = result.toString()
+    }
+
+    fun divisionOperator(firstNumber: Int, secondNumber: Int) {
+        val result = firstNumber / secondNumber
+
+        txtResult.text = result.toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAdd.setOnClickListener{
+            val firstNumber = etFirstNumber.text.toString().toInt()
+            val secondNumber = etSecondNumber.text.toString().toInt()
+
+            addOperator(firstNumber, secondNumber)
+        }
+
+        btnMinus.setOnClickListener{
+            val firstNumber = etFirstNumber.text.toString().toInt()
+            val secondNumber = etSecondNumber.text.toString().toInt()
+
+            minusOperator(firstNumber, secondNumber)
+        }
+
+        btnMultiple.setOnClickListener {
+            val firstNumber = etFirstNumber.text.toString().toInt()
+            val secondNumber = etSecondNumber.text.toString().toInt()
+
+            multipleOperator(firstNumber, secondNumber)
+        }
+
+        btnDivide.setOnClickListener {
+            val firstNumber = etFirstNumber.text.toString().toInt()
+            val secondNumber = etSecondNumber.text.toString().toInt()
+
+            divisionOperator(firstNumber, secondNumber)
+        }
     }
 }
